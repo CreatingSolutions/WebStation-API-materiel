06c788f4635765d8ff986b60323ea67b70a759e5
damiensauv <damiensauvalle@gmail.com>
2019-03-23 14:15:30 +0100

    fix

--- src/main/java/webstationapi/Entity/StuffBook.java
+++ src/main/java/webstationapi/Entity/StuffBook.java
@@ -5,12 +5,17 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Getter
 @Setter
 public class StuffBook {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private int userId;
